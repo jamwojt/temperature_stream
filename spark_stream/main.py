b41be7fa809d67d9786adf4c222af8ba8ea1c90e
jamwojt <jamwojt@gmail.com>
2025-01-03 15:06:15 +0100

    create basic connection
    calling the API endpoint passes data to kafka, and spark is connected to the topic and dixplays it in console

--- spark_stream/main.py
+++ spark_stream/main.py
@@ -1,10 +1,35 @@
-import logging
 import time
 
+from kafka.admin import KafkaAdminClient
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.types import ByteType, StringType, StructField, StructType
 
+
+KAFKA_BROKER = "kafka:9092"
+KAFKA_TOPIC = "sensor_queue"
+
+
+def connect_admin_client(broker: str, retries: int, delay: int) -> KafkaAdminClient:
+    for i in range(retries):
+        try:
+            kafka_admin = KafkaAdminClient(bootstrap_servers=broker)
+            return kafka_admin
+        except:
+            print(f"attempt {i + 1}/{retries}: failed to connect to kafka")
+            time.sleep(delay)
+
+
+def wait_for_kafka(broker: str, topic: str, retries: int, delay: int) -> None:
+    kafka_admin = connect_admin_client(broker, retries, delay)
+    for i in range(retries):
+        if topic in kafka_admin.list_topics():
+            return None
+        print(f"attempt {i + 1}/{retries}: waiting for topic to exist")
+        time.sleep(delay)
+
+
+wait_for_kafka(KAFKA_BROKER, KAFKA_TOPIC, retries=12, delay=5)
 
 spark = (
     SparkSession.builder.appName("temp_stream")
@@ -12,12 +37,10 @@ spark = (
     .getOrCreate()
 )
 
-kafka_broker = "kafka:9092"
-
 
 listener_df = (
     spark.readStream.format("kafka")
-    .option("kafka.bootstrap.servers", kafka_broker)
+    .option("kafka.bootstrap.servers", KAFKA_BROKER)
     .option("subscribe", "sensor_queue")
     .option("startingOffsets", "earliest")
     .load()
@@ -28,12 +51,7 @@ transform_df = listener_df.selectExpr(
     "CAST(key AS STRING)", "CAST(value AS STRING)", "timestamp"
 )
 
-transform_df\
-    .writeStream\
-    .outputMode("append")\
-    .format("console")\
-    .start()
+transform_df.writeStream.outputMode("append").format("console").start()
 
 print("sleeping")
 time.sleep(60)
-
--- stream_api/main.py
+++ stream_api/main.py
@@ -1,6 +1,41 @@
+import json
+import time
+
 from fastapi import FastAPI
 from kafka import KafkaProducer
-import time
+from kafka.admin import KafkaAdminClient, NewTopic
+from kafka.errors import NoBrokersAvailable
+
+
+def create_kafka_topic(broker: str, topic: str):
+    kafka_admin = KafkaAdminClient(bootstrap_servers=broker)
+
+    while True:
+        try:
+            if topic not in kafka_admin.list_topics():
+                kafka_admin.create_topics(
+                    [
+                        NewTopic(name=topic, num_partitions=1, replication_factor=1),
+                    ]
+                )
+                break
+
+        except:
+            print("waiting for kafka to start")
+            time.sleep(2)
+
+    kafka_admin.close()
+
+
+def create_kafka_producer(broker: str, retries: int, delay: int) -> KafkaProducer:
+    for i in range(retries):
+        try:
+            producer = KafkaProducer(bootstrap_servers=broker)
+            return producer
+        except NoBrokersAvailable:
+            print(f"attempt {i + 1}/{retries}: Cannot find kafka broker")
+            time.sleep(delay)
+    raise Exception("Cannot connect producer to kafka broker")
 
 
 app = FastAPI()
@@ -8,21 +43,9 @@ app = FastAPI()
 kafka_broker = "kafka:9092"
 kafka_topic = "sensor_queue"
 
-connected = False
-connecting_time = 0
+kafka_producer = None
+kafka_topic_exists = False
 
-while connecting_time <= 60 and not connected:
-    try:
-        print(f"connecting to kafka... ({connecting_time})")
-        kafka_producer = KafkaProducer(bootstrap_servers=kafka_broker)
-        print("connected to the kafka broker")
-        connected = True
-    except:
-        connected += 2
-        time.sleep(2)
-
-if connecting_time > 60:
-    print("couldn't connect with kafka broker")
 
 @app.get("/")
 def home():
@@ -32,7 +55,19 @@ def home():
 
 @app.post("/pass-data/{temp}/{hum}")
 def pass_data(temp: float, hum: float):
-    data = {"temperature": float(temp), "humidity": float(hum)}
+    global kafka_producer
+    global kafka_topic_exists
+
+    if not kafka_topic_exists:
+        create_kafka_topic(kafka_broker, kafka_topic)
+        kafka_topic_exists = True
+
+    if kafka_producer is None:
+        kafka_producer = create_kafka_producer(kafka_broker, retries=12, delay=5)
+
+    data = json.dumps({"temperature": float(temp), "humidity": float(hum)}).encode(
+        "utf-8"
+    )
     kafka_producer.send(topic=kafka_topic, value=data)
 
 
